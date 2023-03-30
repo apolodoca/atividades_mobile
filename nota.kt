dafb282377b3d787869222d2fba9ec8979bb0a06
apolodoca <89225317+apolodoca@users.noreply.github.com>
2023-03-30 09:29:11 -0300

    Add files via upload

---
+++ nota.kt
@@ -0,0 +1,18 @@
+class aluno(var nome: String, var nota: Double){ 
+} 
+    
+
+
+
+
+fun main() {
+    val apolo = aluno("apolo", 3.0)
+    println(apolo.nome)
+    println(apolo.nota)
+
+
+
+
+
+
+} 
